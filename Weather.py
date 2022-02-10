1ae52a79640785f1a883482662e552cd19103fdb
Harsh Koli <harshkoli201@gmail.com>
2022-02-10 20:42:32 +0530

    Search Bar added

--- Weather.py
+++ Weather.py
@@ -3,6 +3,22 @@ from tkinter import *
 
 app = Tk()
 app.title("World Weather")
-app.geometry('500x500')
-app.mainloop()
+app.geometry('700x500')
+app.configure(bg='grey')
 
+city_name = StringVar()
+city_entry = Entry(app, textvariable=city_name, width=50)
+city_entry.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+app.mainloop()
