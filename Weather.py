e0a8d7579540f6f2529bd28b87e2edc1b73490b1
Harsh Koli <harshkoli201@gmail.com>
2022-02-10 21:06:40 +0530

    Frontend

--- Weather.py
+++ Weather.py
@@ -6,19 +6,27 @@ app.title("World Weather")
 app.geometry('700x500')
 app.configure(bg='blue')
 
+def search():
+    pass
+
 city_name = StringVar()
 city_entry = Entry(app, textvariable=city_name, width=45)
 city_entry.pack(pady=20)
 
-button = Button(app, text = "Search Weather", width= 15)
+button = Button(app, text = "Search Weather", command=search,  width= 15)
 button.pack(pady=10)
 
+location_name = Label(app, text ="", font=('bold',20))
+location_name.pack(pady=10)
 
+# image = Label(app, bitmap='')
+# image.pack()
 
+Temp_lbl= Label(app, text="")
+Temp_lbl.pack(pady=10)
 
-
-
-
+weather = Label(app, text="")
+weather.pack(pady=10)
 
 
 app.mainloop()
