5e515354636476f76bb12373906bc0eadce2624c
Harsh Koli <harshkoli201@gmail.com>
2022-02-10 20:49:58 +0530

    Button Added

--- Weather.py
+++ Weather.py
@@ -4,14 +4,14 @@ from tkinter import *
 app = Tk()
 app.title("World Weather")
 app.geometry('700x500')
-app.configure(bg='grey')
+app.configure(bg='blue')
 
 city_name = StringVar()
-city_entry = Entry(app, textvariable=city_name, width=50)
-city_entry.pack()
+city_entry = Entry(app, textvariable=city_name, width=45)
+city_entry.pack(pady=20)
 
-
-
+button = Button(app, text = "Search Weather", width= 15)
+button.pack(pady=10)
 
 
 
